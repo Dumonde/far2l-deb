04db4b769a719cad45f75b71e67ee185b9109e0d
Dumonde <dumonde@bk.ru>
2022-09-30 15:35:16 +0500

    Update lxc-far2l-portable.sh

--- portable/lxc-far2l-portable.sh
+++ portable/lxc-far2l-portable.sh
@@ -5,14 +5,14 @@ sudo lxc-stop -n far2l
 sudo lxc-destroy -n far2l
 sudo rm -rf ~/far2l_portable.run
 # here we go
-sudo lxc-create -t download -n far2l -- --keyserver hkp://p80.pool.sks-keyservers.net:80 -d ubuntu -r trusty -a amd64
+sudo lxc-create -t download -n far2l -- -d ubuntu -r kinetic -a amd64
 sudo lxc-start -n far2l -d
 sleep 2
 sudo lxc-attach -n far2l -- sudo apt install -y wget
-sudo lxc-attach -n far2l -- wget https://github.com/unxed/far2l-deb/raw/master/portable/make_far2l_portable_on_ubuntu_14_04.sh
-sudo lxc-attach -n far2l -- chmod +x make_far2l_portable_on_ubuntu_14_04.sh
-sudo lxc-attach -n far2l -- ./make_far2l_portable_on_ubuntu_14_04.sh
+sudo lxc-attach -n far2l -- wget https://github.com/Dumonde/far2l-deb/raw/master/portable/make_far2l_portable_on_ubuntu.sh
+sudo lxc-attach -n far2l -- chmod +x make_far2l_portable_on_ubuntu.sh
+sudo lxc-attach -n far2l -- ./make_far2l_portable_on_ubuntu.sh
 sudo chmod +r -R /var/lib/lxc/far2l
-sudo cp /var/lib/lxc/far2l/rootfs/home/$LOGNAME/far2l/far2l/build/far2l_portable.run ~
+sudo cp /var/lib/lxc/far2l/rootfs/home/root/far2l/far2l/build/far2l_portable.run ~
 sudo lxc-stop -n far2l
 sudo lxc-destroy -n far2l
